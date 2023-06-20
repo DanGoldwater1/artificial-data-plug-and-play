1daaed9c436d848d48d6a72723a88b8e8bf613bc
Daniel Goldwater <daniel.goldwater1@nhs.net>
2023-06-20 09:44:08 +0100

    Produced basic plots and added desc

--- src/main.py
+++ src/main.py
@@ -1,38 +1,29 @@
 #%%
+
 import os
 from data_ingestion import preprocessing
 import pathlib
 import pandas as pd
 import pyarrow
 import duckdb
-
-conn = duckdb.connect()
-
-import duckdb
-
-# Establish a connection to DuckDB
+import pathlib 
+import sys
 conn = duckdb.connect()
 
 
-file_location = pathlib.Path('data/')
-# Load the Parquet file into a DuckDB table
-conn.execute("CREATE TABLE my_table AS SELECT * FROM parquet_scan('data.parquet')")
 
-# Now you can run queries on the data. Here are some simple examples:
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_dir)
 
-# 1. Select all rows from the table
-result = conn.execute("SELECT * FROM my_table")
-print(result.fetchall())
+conn = duckdb.connect('my_db.db')
 
-# 2. Get count of rows in the table
-result = conn.execute("SELECT COUNT(*) FROM my_table")
-print(result.fetchone())
+parquet_path =  pathlib.Path('data/artificial_hes_ae_202302_v1_sample/sample.parquet')
+if os.path.exists(parquet_path):
+    print('yes')
+print(os.listdir('data/artificial_hes_ae_202302_v1_sample'))
 
-# 3. Get a distinct value count of a specific column (replace 'column_name' with your actual column name)
-result = conn.execute("SELECT COUNT(DISTINCT column_name) FROM my_table")
-print(result.fetchone())
+# current_dir = os.path.dirname(os.path.abspath(__file__))))
+# conn.execute(f"CREATE TABLE my_table AS SELECT * FROM parquet_scan('{parquet_path}')")
 
-# Don't forget to close the connection when you're done
-conn.close()
-
+# result = conn.execute("SELECT * FROM my_table").fetchall()
 
